63b42d313ed2a2ee08dd49ac55dfeacfefd5f779
leo-chartier <leo.chartier@algosup.com>
2023-03-23 13:54:17 +0100

    add cafeteria and classroom

---
+++ tilesets/chair.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.0" name="chair" tilewidth="32" tileheight="32" tilecount="4" columns="4">
+ <image source="../sprites/decors/objects/chair tileset.png" width="128" height="32"/>
+</tileset>
---
+++ tilesets/dinning vertical.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.0" name="dinning vertical" tilewidth="32" tileheight="64" tilecount="1" columns="1">
+ <image source="../sprites/decors/objects/dinning vertical.png" width="32" height="64"/>
+</tileset>
