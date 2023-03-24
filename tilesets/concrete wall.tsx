7c263d44518b388759df7e587d1bea0ea05b3df3
leo-chartier <leo.chartier@algosup.com>
2023-03-24 15:10:22 +0100

    add corridor level

---
+++ tilesets/concrete wall.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.0" name="concrete wall" tilewidth="32" tileheight="32" tilecount="2" columns="1">
+ <image source="../sprites/decors/walls/concrete wall.png" width="32" height="64"/>
+</tileset>
