572d3b76f80dcc165ad336fbbeb97fd33dd9d644
leo-chartier <leo.chartier@algosup.com>
2023-03-27 11:28:09 +0200

    add playground from level 1

---
+++ tilesets/swing.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.0" name="swing" tilewidth="32" tileheight="32" tilecount="6" columns="3">
+ <image source="../sprites/decors/objects/swing.png" width="96" height="64"/>
+</tileset>
